02708fdadd47072bf6853f1d78da338f161073cf
v.pintia <pintya@i.ua>
2018-08-21 22:20:53 +0300

    Add Laravel Passport

--- app/Client.php
+++ app/Client.php
@@ -3,8 +3,18 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Passport\HasApiTokens;
 
 class Client extends Model
 {
-    //
+    use HasApiTokens, Notifiable;
+
+    protected $fillable = [
+        'name', 'email', 'password', 'address', 'phone', 'avatar', 'description'
+    ];
+
+    protected $hidden = [
+        'password'
+    ];
 }
